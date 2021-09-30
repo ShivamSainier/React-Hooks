d98c3fa3cc3592c50c3cd393601843ee52f0e020
shivam saini <shivamsainier98@gmail.com>
2021-09-30 07:46:37 +0530

    Counter Using prev State

--- react_hooks/src/Hooks/Counter_useState.js
+++ react_hooks/src/Hooks/Counter_useState.js
@@ -3,13 +3,14 @@ import React, { useState } from 'react'
 function Counter_useState() {
     const [state,setState]=useState(0)
 
-    const increment=()=>{
-        setState(state+1)
-    }
+   
     return (
         <div>
-            <button onClick={increment}>Count</button>
             <h1>{state}</h1>
+            <button onClick={()=>{setState(state+1)}}>Increment</button>
+            <button onClick={()=>{setState(state-1)}}>Deccrement</button>
+            <button onClick={()=>{setState(prev=>prev+5)}}>Increment by 5</button>
+            
         </div>
     )
 }
