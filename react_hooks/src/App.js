40686ef4ac43101f08afc3fe09cff4bc28e83ae2
shivam saini <shivamsainier98@gmail.com>
2021-10-01 11:09:56 +0530

    Counter with UseReducer

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Array_Hooks from './Hooks/Array_Hooks';
 import UseEffect from './Hooks/UseEffect';
+import UseReducer from './Hooks/UseReducer';
 
 function App() {
   return (
     <div className="App">
-      <UseEffect />
+      <UseReducer />
     </div>
   );
 }
