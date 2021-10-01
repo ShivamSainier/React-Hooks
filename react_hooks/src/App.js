5c12b578893a92d1e1c95feb7c8ec5cb9ebeb873
shivam saini <shivamsainier98@gmail.com>
2021-10-01 11:28:08 +0530

    Hendle Input via useReducer

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Array_Hooks from './Hooks/Array_Hooks';
 import UseEffect from './Hooks/UseEffect';
 import UseReducer from './Hooks/UseReducer';
+import Input_useReducer from './Hooks/Input_useReducer';
 
 function App() {
   return (
     <div className="App">
-      <UseReducer />
+     <Input_useReducer />
     </div>
   );
 }
---
+++ react_hooks/src/Hooks/Input_useReducer.js
@@ -0,0 +1,21 @@
+import React,{useReducer} from 'react'
+
+function Input_useReducer() {
+    const reducer=(state,action)=>{
+        state=action
+        return state
+        
+    }
+    const [value,setValue]=useReducer(reducer,"helo")
+    console.log(value)
+    return (
+        <div>
+            <h1>{value}</h1>
+            <input type="text" placeholder="Input" onChange={(event)=>{
+                setValue(event.target.value)
+            }}></input>
+        </div>
+    )
+}
+
+export default Input_useReducer
